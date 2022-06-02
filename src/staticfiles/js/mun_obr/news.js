1ac65ce0329c4977c1faf44786c9a27d5a6608fd
justbegan <72058184+justbegan@users.noreply.github.com>
2022-06-03 00:37:23 +0900

    add modal
    add modal

--- src/staticfiles/js/mun_obr/news.js
+++ src/staticfiles/js/mun_obr/news.js
@@ -4,7 +4,33 @@ new Vue({
     data: { 
         base_url: "",
         news_json:[],
+        selected_news:"",
     
+
+    },
+
+    methods: {
+
+        open_news:function(e,news_id){
+        const  vm = this
+        var d = vm.news_json
+        vm.selected_news = d.filter(function(val) {
+        return val.id == news_id;})[0]
+        console.log(vm.selected_news)
+        vm.get_modal("main_Modal")
+
+        },
+        get_modal:function(type){
+            const vm = this
+         
+            var myModal = new bootstrap.Modal(document.getElementById(type), {
+              keyboard: false
+            })
+            myModal.show()
+    
+    
+          }
+        
 
     },
 
